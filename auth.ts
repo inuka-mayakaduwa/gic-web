17c1bca8bcc95e32b91235c39613929554407251
Inuka Mayakaduwa <lunoxx.dev@gmail.com>
2025-12-10 18:21:59 +0530

    Added trusted host

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { getPrisma } from "@/lib/prisma"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     secret: process.env.AUTH_SECRET,
+    trustHost: true,
     providers: [
         Credentials({
             name: "OTP",
